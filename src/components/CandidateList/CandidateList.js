e75095003340bc45f369981b85a75596d4440f06
M-MUGUNTHAN <52280716+M-MUGUNTHAN@users.noreply.github.com>
2021-01-04 17:34:27 +0530

    commit

--- src/components/CandidateList/CandidateList.js
+++ src/components/CandidateList/CandidateList.js
@@ -1,6 +1,7 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const CandidateList = (props) => {
+    const[loading,setLoading]=useState(true);
     useEffect(()=>{
             fetch("https://s3-ap-southeast-1.amazonaws.com/he-public-data/users49b8675.json")
             .then(response=>response.json())
@@ -15,8 +16,12 @@ const CandidateList = (props) => {
                     );
                 })
              localStorage.setItem("candidateList",JSON.stringify(data))
+             setLoading(false);
             })
     },[])
+    if(loading===true){
+        return <div>Loading...</div>
+    }
     return (
        <>
        {props.children}
